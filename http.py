b43d29499c067f1a5f0b2e14aee5b794626a1a26
Aleksandr6669 <Gabreil666@gmail.com>
2022-04-27 13:08:25 +0300

    master

--- http.py
+++ http.py
@@ -3,15 +3,16 @@ import socketserver
 from threading import Thread
 import os
 
+
+
 def server_thread(port):
     handler = http.server.SimpleHTTPRequestHandler
     with socketserver.TCPServer(("", port), handler) as httpd:
         httpd.serve_forever()
-
 if __name__ == '__main__':
 
-    port = 8082
+    port = 80
     print("Starting server at port %d" % port)
-    #os.chdir("/media/pi/Date/www/www")
+    #os.chdir("/home/www/www")
     os.chdir("www")
     Thread(target=server_thread, args=(port,)).start()
--- https.py
+++ https.py
@@ -2,16 +2,24 @@ import http.server
 import socketserver
 from threading import Thread
 import os
+import ssl
+
+
+
+script_dir = os.path.dirname(os.path.realpath(__file__))
+keypath = os.path.join(script_dir, 'private.key')
+certpath = os.path.join(script_dir, 'certificate.crt')
 
 def server_thread(port):
     handler = http.server.SimpleHTTPRequestHandler
     with socketserver.TCPServer(("", port), handler) as httpd:
+        httpd.socket = ssl.wrap_socket(
+        httpd.socket, keyfile=keypath, certfile=certpath, server_side=True)
         httpd.serve_forever()
-
 if __name__ == '__main__':
 
-    port = 8082
+    port = 443
     print("Starting server at port %d" % port)
-    #os.chdir("/media/pi/Date/www/www")
+    #os.chdir("/home/www/www")
     os.chdir("www")
     Thread(target=server_thread, args=(port,)).start()
